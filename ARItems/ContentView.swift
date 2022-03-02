ee2600a2b29566df3781bc4582b529d27b91fde3
efemazlumoglu <efe.mazlumoglu@gmail.com>
2022-03-02 23:01:30 +0300

    model view struct implemented

--- ARItems/ContentView.swift
+++ ARItems/ContentView.swift
@@ -9,8 +9,13 @@ import SwiftUI
 import RealityKit
 
 struct ContentView : View {
+    var models: [String] = ["AirForce", "PegasusTrail"]
+    
     var body: some View {
-        return ARViewContainer().edgesIgnoringSafeArea(.all)
+        ZStack(alignment: .bottom) {
+            ARViewContainer()
+            ModelPickerView(models: self.models)
+        }
     }
 }
 
@@ -20,18 +25,35 @@ struct ARViewContainer: UIViewRepresentable {
         
         let arView = ARView(frame: .zero)
         
-        // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Experience.loadBox()
-        
-        // Add the box anchor to the scene
-        arView.scene.anchors.append(boxAnchor)
-        
         return arView
         
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {}
     
+}
+
+struct ModelPickerView: View {
+    var models: [String]
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            HStack(spacing: 30) {
+                ForEach(0 ..< self.models.count) {
+                    index in
+                    Button(action: {
+                        print("DEBUG: selected model with the name: \(self.models[index])")
+                    }) {
+                        Image(uiImage: UIImage(named: self.models[index])!).resizable().frame(height: 80).aspectRatio(1/1, contentMode: .fit)
+                            .background(Color.white)
+                            .cornerRadius(12)
+                    }
+                    .buttonStyle(PlainButtonStyle())
+                }
+            }
+        }
+        .padding(20)
+        .background(Color.black).opacity(0.5)
+    }
 }
 
 #if DEBUG
