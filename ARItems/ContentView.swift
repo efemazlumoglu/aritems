eb9bca5814e05e369b347b01a1072b5ab2cbda44
efemazlumoglu <efe.mazlumoglu@gmail.com>
2022-03-03 10:34:23 +0300

    initialize to the phoee and start testing

--- ARItems/ContentView.swift
+++ ARItems/ContentView.swift
@@ -7,10 +7,13 @@
 
 import SwiftUI
 import RealityKit
+import ARKit
 
 struct ContentView : View {
     
     @State private var isPlacementEnabled = false
+    @State private var selectedModel: String?
+    @State private var modelConfirmedForPlacement: String?
     
     private var models: [String] = {
         
@@ -34,11 +37,11 @@ struct ContentView : View {
         
         ZStack(alignment: .bottom) {
             
-            ARViewContainer()
+            ARViewContainer(modelConfirmedForPlacement: self.$modelConfirmedForPlacement)
             if self.isPlacementEnabled {
-                PlacementButtonView(isPlacementEnabled: self.$isPlacementEnabled)
+                PlacementButtonView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, modelConfirmedForPlacement: self.$modelConfirmedForPlacement)
             } else {
-                ModelPickerView(isPlacementEnabled: self.$isPlacementEnabled, models: self.models)
+                ModelPickerView(isPlacementEnabled: self.$isPlacementEnabled, selectedModel: self.$selectedModel, models: self.models)
             }
             
         }
@@ -48,21 +51,49 @@ struct ContentView : View {
 
 struct ARViewContainer: UIViewRepresentable {
     
+    @Binding var modelConfirmedForPlacement: String?
+    
     func makeUIView(context: Context) -> ARView {
         
         let arView = ARView(frame: .zero)
+        
+        let config = ARWorldTrackingConfiguration()
+        config.planeDetection = [.horizontal, .vertical]
+        config.environmentTexturing = .automatic
+        
+        if ARWorldTrackingConfiguration.supportsSceneReconstruction(.mesh) {
+            config.sceneReconstruction = .mesh
+        }
+        
+        arView.session.run(config)
         
         return arView
         
     }
     
-    func updateUIView(_ uiView: ARView, context: Context) {}
+    func updateUIView(_ uiView: ARView, context: Context) {
+        if let modelName = self.modelConfirmedForPlacement {
+            print("DEBUG: adding model to scene \(modelName)")
+            
+            let filename = modelName + ".usdz"
+            let modelEntity = try! ModelEntity.loadModel(named: filename)
+            let anchorEntity = AnchorEntity(plane: .any)
+            anchorEntity.addChild(modelEntity)
+            
+            uiView.scene.addAnchor(anchorEntity)
+            
+            DispatchQueue.main.async {
+                self.modelConfirmedForPlacement = nil
+            }
+        }
+    }
     
 }
 
 struct ModelPickerView: View {
     
     @Binding var isPlacementEnabled: Bool
+    @Binding var selectedModel: String?
     
     var models: [String]
     
@@ -74,6 +105,7 @@ struct ModelPickerView: View {
                     index in
                     Button(action: {
                         print("DEBUG: selected model with the name: \(self.models[index])")
+                        self.selectedModel = self.models[index]
                         self.isPlacementEnabled = true
                     }) {
                         Image(uiImage: UIImage(named: self.models[index])!).resizable().frame(height: 80).aspectRatio(1/1, contentMode: .fit)
@@ -92,6 +124,8 @@ struct ModelPickerView: View {
 struct PlacementButtonView: View {
     
     @Binding var isPlacementEnabled: Bool
+    @Binding var selectedModel: String?
+    @Binding var modelConfirmedForPlacement: String?
     
     var body: some View {
         HStack {
@@ -105,6 +139,7 @@ struct PlacementButtonView: View {
             // Confirm Button
             Button(action: {
                 print("DEBUG: Confirm model placement.")
+                self.modelConfirmedForPlacement = self.selectedModel
                 self.resetPlacementParameters()
             }) {
                 Image(systemName: "checkmark").frame(width: 60, height: 60).font(.title).background(Color.white).opacity(0.75).cornerRadius(30).padding(20)
@@ -114,6 +149,7 @@ struct PlacementButtonView: View {
     
     func resetPlacementParameters() {
         self.isPlacementEnabled = false
+        self.selectedModel = nil
     }
 }
 
